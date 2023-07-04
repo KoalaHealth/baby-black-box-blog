eca4920ec9925d07eaf9b3b6e0914ed19004d1ea
motte <michaelotte1@gmail.com>
2023-07-03 20:15:23 -0700

    Test

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -12,7 +12,7 @@
 // Hide Header on on scroll down
     var didScroll;
     var lastScrollTop = 0;
-    var delta = 320;
+    var delta = 340;
     var navbarHeight = $('nav').outerHeight();
 
     $(window).scroll(function(event){
